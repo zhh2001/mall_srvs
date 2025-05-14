e9264abff331ed88d10c37dc39bef5155691886d
Zhang Henghua <1652709417@qq.com>
2025-05-14 14:36:05 +0800

    初始化商品服务

---
+++ goods_srv/initialize/db.go
@@ -0,0 +1,47 @@
+package initialize
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+
+	"mall_srvs/goods_srv/global"
+)
+
+func InitDB() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Silent,
+			Colorful:      true,
+		},
+	)
+
+	mysqlInfo := global.ServerConfig.MysqlInfo
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		mysqlInfo.User,
+		mysqlInfo.Password,
+		mysqlInfo.Host,
+		mysqlInfo.Port,
+		mysqlInfo.Name,
+	)
+
+	// 全局模式
+	var err error
+	global.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
