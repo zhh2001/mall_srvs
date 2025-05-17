4d04e6a69fbe17df89927b3d7ff1f8b09ff2e321
Zhang Henghua <1652709417@qq.com>
2025-05-17 13:30:30 +0800

    本地数据库事务解决库存扣减的失败问题

--- inventory_srv/handler/inventory.go
+++ inventory_srv/handler/inventory.go
@@ -39,3 +39,25 @@ func (s *InventoryServer) InvDetail(ctx context.Context, req *proto.GoodsInvInfo
 		Num:     inv.Stocks,
 	}, nil
 }
+
+func (s *InventoryServer) Sell(ctx context.Context, req *proto.SellInfo) (*emptypb.Empty, error) {
+	// 扣减库存，本地事务，数据一致性
+	tx := global.DB.Begin()
+	for _, goodsInfo := range req.GoodsInfo {
+		var inv model.Inventory
+		if result := global.DB.First(&inv, goodsInfo.GetGoodsId()); result.RowsAffected == 0 {
+			tx.Rollback()
+			return nil, status.Errorf(codes.InvalidArgument, "没有库存信息")
+		}
+		// 判断库存是否充足
+		if inv.Stocks < goodsInfo.Num {
+			tx.Rollback()
+			return nil, status.Errorf(codes.ResourceExhausted, "库存不足")
+		}
+		// 扣减
+		inv.Stocks = inv.Stocks - goodsInfo.Num
+		tx.Save(&inv)
+	}
+	tx.Commit()
+	return &emptypb.Empty{}, nil
+}
