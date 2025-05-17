62b9dc115376421d5a0f400d45897f5ea223c924
Zhang Henghua <1652709417@qq.com>
2025-05-17 11:40:05 +0800

    库存服务的表结构设计

---
+++ inventory_srv/model/main/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+
+	"mall_srvs/inventory_srv/model"
+)
+
+func genMD5(code string) string {
+	MD5 := md5.New()
+	MD5.Write([]byte(code))
+	return hex.EncodeToString(MD5.Sum(nil))
+}
+
+func main() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+
+	dsn := "root:123456@tcp(10.120.21.77:3306)/shop_inventory_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(
+		&model.Inventory{},
+	)
+	if err != nil {
+		panic(err)
+	}
+}
